165f6e6d5ec4a5c8daa966cba69789bf58ce367d
unknown <faezehjafarpour75@gmail.com>
2022-06-05 21:57:02 +0430

    identity

---
+++ IDP/Controllers/ClientsController.cs
@@ -0,0 +1,53 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDP.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly ConfigurationDbContext _configuration;
+        public ClientsController(ConfigurationDbContext configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            var clients = _configuration.Clients.ToList();
+            return View(clients);
+        }
+
+        public IActionResult Add()
+        {
+            _configuration.Clients.Add(new IdentityServer4.EntityFramework.Entities.Client
+            {
+                ClientId = "ApiWeatherId",
+                ClientSecrets = new List<IdentityServer4.EntityFramework.Entities.ClientSecret>()
+                 {
+                      new IdentityServer4.EntityFramework.Entities.ClientSecret
+                      {
+                           Value = "123456".Sha256()
+                      }
+                 },
+                AllowedGrantTypes = new List<IdentityServer4.EntityFramework.Entities.ClientGrantType>()
+                 {
+                      new IdentityServer4.EntityFramework.Entities.ClientGrantType()
+                      {
+                           GrantType ="ClientCredentials"
+                      }
+                 },
+                AllowedScopes = new List<IdentityServer4.EntityFramework.Entities.ClientScope>()
+                 {
+                     new IdentityServer4.EntityFramework.Entities.ClientScope()
+                     {
+                          Scope="ApiWeather"
+                     }
+                 }
+
+            });
+            _configuration.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- IDP/Program.cs
+++ IDP/Program.cs
@@ -1,5 +1,6 @@
 using IdentityServer4.Models;
 using IDP.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 using static IdentityServer4.IdentityServerConstants;
 
@@ -9,13 +10,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 // identity server
+string ConnectionString = @"data source =.; initial catalog = SSOdb";
+var miAssembly = typeof(Program).GetType().Assembly.GetName().Name;
+
 builder.Services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddTestUsers(ConfigIdentityServer.GetTestUsers())
-                .AddInMemoryIdentityResources(ConfigIdentityServer.GetIdentityServer())
-                .AddInMemoryApiResources(ConfigIdentityServer.GetApiResource())
-                .AddInMemoryClients(ConfigIdentityServer.GetClient())
-                .AddInMemoryApiScopes(ConfigIdentityServer.GetApiScopes());
+                .AddConfigurationStore(option =>
+                {
+                    option.ConfigureDbContext = b =>
+                     b.UseSqlServer(ConnectionString, sql => sql.MigrationsAssembly(miAssembly));
+                })
+                .AddOperationalStore(option =>
+                {
+
+                    option.ConfigureDbContext = b =>
+                    b.UseSqlServer(ConnectionString, sql => sql.MigrationsAssembly(miAssembly));
+                });
+//IdentityServer4.EntityFramework.DbContexts.ConfigurationDbContext
+//IdentityServer4.EntityFramework.DbContexts.PersistedGrantDbContext
+
+
+//.AddInMemoryIdentityResources(ConfigIdentityServer.GetIdentityServer())
+//.AddInMemoryApiResources(ConfigIdentityServer.GetApiResource())
+//.AddInMemoryClients(ConfigIdentityServer.GetClient())
+//.AddInMemoryApiScopes(ConfigIdentityServer.GetApiScopes());
 
 //
 
@@ -24,7 +43,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler(" / Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
